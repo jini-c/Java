6f6f483988d35e020943ddbc5302b20c2c403594
박창진 <pwh1011@naver.com>
2025-05-29 09:18:41 +0900

    Example of FileWriter
    Save keyboard input to a file

---
+++ Input Output Streams File Input Output/FileWriterEx.java
@@ -0,0 +1,19 @@
+package ch8;
+
+import java.io.*;
+import java.util.*;
+
+public class FileWriterEx {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		FileWriter fout = null;
+		int c;
+		try {
+			fout 099,ㅏ[]
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
